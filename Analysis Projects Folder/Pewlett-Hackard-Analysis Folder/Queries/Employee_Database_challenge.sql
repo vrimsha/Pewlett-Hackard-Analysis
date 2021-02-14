bcde9c3ce7ab81752c03dca9a50d149eb9c22bac
Veronika R <veronikars7@Veronikas-iMac.local>
2021-02-14 18:50:29 -0500

    Deliverable 2: The Employees Eligible for the Mentorship Program

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Database_challenge.sql
@@ -39,3 +39,17 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT (emp_no) DESC;
+
+--Deliverable 2: The Employees Eligible for the Mentorship Program
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, t.titles
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+AND de.to_date = '9999-01-01'
+AND t.to_date = '9999-01-01'
+ORDER BY e.emp_no;
